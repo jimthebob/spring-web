21e932fd0a334d6dfed4cfabf86b20e970ba51d0
Firstname Lastname <Firstname Lastname@LUKPC304.lhasalimited.org>
2017-10-30 11:40:51 +0000

    first

---
+++ gs-rest-service/gs-rest-service-initial/src/main/java/hello/GreetingController.java
@@ -0,0 +1,29 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
+/* ---------------------------------------------------------------------*
+ * This software is the confidential and proprietary
+ * information of Lhasa Limited
+ * Granary Wharf House, 2 Canal Wharf, Leeds, LS11 5PS
+ * ---
+ * No part of this confidential information shall be disclosed
+ * and it shall be used only in accordance with the terms of a
+ * written license agreement entered into by holder of the information
+ * with LHASA Ltd.
+ * --------------------------------------------------------------------- */
